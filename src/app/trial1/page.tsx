b91f955dc3cd075e2014f2d875fd00d01e24bd06
bonanza127 <hokhok7676@gmail.com>
2025-12-24 13:06:38 +0900

    fix: update trial1 token expiration to 2026/01/15

--- src/app/trial1/page.tsx
+++ src/app/trial1/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from 'next/navigation';
 
 // テスト用一号（Trial #1）のマジックリンクにリダイレクト
-// 有効期限: 2025/01/15 23:59:59 JST
-const MAGIC_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoibWFnaWNfbGluayIsIm5hbWUiOiJUcmlhbCAjMSIsImVtYWlsIjoidHJpYWwxQGFpY2hlY2tlcnMubmV0IiwiaXNfYWRtaW4iOnRydWUsImV4cCI6MTczNjk1MzE5OX0.wl0yHAGEM1PSftiovOo7-B82JzMg03SYS23eI6YT34o";
+// 有効期限: 2026/01/15 23:59:59 JST
+const MAGIC_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoibWFnaWNfbGluayIsIm5hbWUiOiJUcmlhbCAjMSIsImVtYWlsIjoidHJpYWwxQGFpY2hlY2tlcnMubmV0IiwiaXNfYWRtaW4iOnRydWUsImV4cCI6MTc2ODQ4OTE5OX0.E4cLUmSoKpA7dT5MVl69NG-Q7RqOeTcdZ2GzlMlvxEo";
 
 export default function Trial1Page() {
   redirect(`https://api.aicheckers.net/auth/magic/${MAGIC_TOKEN}`);
